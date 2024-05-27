52c03285398fcbe2a4ea56685734419a4f255d1e
Rafael Colares <rafaelcolares.dev@gmail.com>
2024-05-27 12:42:12 -0300

    Feat: alteração dos cards de projetos.

--- frontend/src/components/Projetos/Projetos.tsx
+++ frontend/src/components/Projetos/Projetos.tsx
@@ -11,48 +11,7 @@ function Projetos() {
       linkGithub: 'link_do_repositorio_github_1',
     },
     {
-      nome: 'App Prestige Manager',
-      descricao: 'App Android - React Native',
-      imagem: image,
-      linkOnline: 'link_do_projeto_online_1',
-      linkGithub: 'link_do_repositorio_github_1',
-    },
-    {
-      nome: 'App Prestige Manager',
-      descricao: 'App Android - React Native',
-      imagem: image,
-      linkOnline: 'link_do_projeto_online_1',
-      linkGithub: 'link_do_repositorio_github_1',
-    },
-    {
-      nome: 'App Prestige Manager',
-      descricao: 'App Android - React Native',
-      imagem: image,
-      linkOnline: 'link_do_projeto_online_1',
-      linkGithub: 'link_do_repositorio_github_1',
-    },
-    {
-      nome: 'App Prestige Manager',
-      descricao: 'App Android - React Native',
-      imagem: image,
-      linkOnline: 'link_do_projeto_online_1',
-      linkGithub: 'link_do_repositorio_github_1',
-    },
-    {
-      nome: 'App Prestige Manager',
-      descricao: 'App Android - React Native',
-      imagem: image,
-      linkOnline: 'link_do_projeto_online_1',
-      linkGithub: 'link_do_repositorio_github_1',
-    },
-    {
-      nome: 'App Prestige Manager',
-      descricao: 'App Android - React Native',
-      imagem: image,
-      linkOnline: 'link_do_projeto_online_1',
-      linkGithub: 'link_do_repositorio_github_1',
-    },
-    {
+      // id missing
       nome: 'App Prestige Manager',
       descricao: 'App Android - React Native',
       imagem: image,
@@ -66,22 +25,35 @@ function Projetos() {
       <h1>Projetos</h1>
       <div className="cards-div-mae">
         {projetos.map((projeto, index) => (
-          <div key={ index } className="cards highlight highlight-cards">
-            <h3>
+          projeto.id ? (
+            <div key={ projeto.id } className="cards highlight highlight-cards">
+              <h3>
+                <a href={ projeto.linkOnline } target="_blank" rel="noopener noreferrer">
+                  {projeto.nome}
+                </a>
+              </h3>
+              <h3>
+                <a href={ projeto.linkGithub } target="_blank" rel="noopener noreferrer">
+                  Source Code
+                </a>
+              </h3>
+              <p>{projeto.descricao}</p>
               <a href={ projeto.linkOnline } target="_blank" rel="noopener noreferrer">
-                {projeto.nome}
+                <img src={ projeto.imagem } alt={ projeto.nome } />
               </a>
-            </h3>
-            <h3>
-              <a href={ projeto.linkGithub } target="_blank" rel="noopener noreferrer">
-                Source Code
-              </a>
-            </h3>
-            <p>{projeto.descricao}</p>
-            <a href={ projeto.linkOnline } target="_blank" rel="noopener noreferrer">
-              <img src={ projeto.imagem } alt={ projeto.nome } />
-            </a>
-          </div>
+            </div>
+          ) : (
+            <div
+              key={ `semId-${index}` }
+              className="cards highlight highlight-cards"
+              style={ {
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center' } }
+            >
+              <h3>Em breve novos projetos!</h3>
+            </div>
+          )
         ))}
       </div>
     </section>
